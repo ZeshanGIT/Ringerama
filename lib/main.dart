7432210dc2e1757f6d2092f0c525d8324581cebd
Seshan <zeshan.nandan@gmail.com>
2019-05-15 00:10:15 +0530

    Working with Playlist

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:demo/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,11 +10,13 @@ class Home extends StatefulWidget {
 enum Cycle { MINUTES, HOURS, DAYS }
 
 class _HomeState extends State<Home> {
+  PageController pageController = new PageController(initialPage: 0);
   int currTab = 0;
   Cycle cycle = Cycle.MINUTES;
   int interval = 10;
 
-  double seekTime = 0.3;
+  double seekTime = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +26,9 @@ class _HomeState extends State<Home> {
         currentIndex: currTab,
         backgroundColor: primaryColor,
         onTap: (i) => setState(() {
+              pageController.animateToPage(i,
+                  duration: Duration(milliseconds: 200),
+                  curve: Curves.easeInOut);
               currTab = i;
             }),
         items: <BottomNavigationBarItem>[
@@ -42,80 +47,184 @@ class _HomeState extends State<Home> {
         backgroundColor: primaryColor,
         title: Text("Ringerama"),
       ),
-      body: LayoutBuilder(
-        builder: (BuildContext buildContext, BoxConstraints boxConstraints) {
-          return Container(
-            margin: EdgeInsets.all(boxConstraints.maxHeight * 0.075),
-            child: LayoutBuilder(
-              builder: (bc, c) {
-                double itemHeight = c.maxHeight / 6;
-                double spacing = c.maxHeight / 24;
-                double w = c.maxWidth;
-                return Stack(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          buildCurrentRing(w, itemHeight, spacing / 1.5),
-                          SizedBox(height: spacing),
-                          buildNextRing(w, itemHeight, spacing / 1.5),
-                          SizedBox(height: spacing),
-                          buildSelectCycle(w, itemHeight, spacing),
-                          SizedBox(height: spacing),
-                          buildPlayingNow(w, itemHeight, spacing / 1.5),
-                          SizedBox(height: spacing),
-                          buildPlayerControls(w, itemHeight, spacing),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          );
-        },
+      body: PageView(
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          buildHome(),
+          LayoutBuilder(
+            builder: (build, bcon) {
+              double k = bcon.maxHeight * 0.02;
+              return Padding(
+                padding: EdgeInsets.fromLTRB(k, k, k, 0),
+                child: LayoutBuilder(
+                  builder: (bc, c) {
+                    double itemHeight = c.maxHeight / 8;
+                    if (itemHeight > 70) itemHeight = 70;
+                    double spacing = c.maxHeight / 24;
+                    double w = c.maxWidth;
+                    return Stack(
+                      children: <Widget>[
+                        ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (bc, i) => Card(
+                                color: Colors.black,
+                                child: Container(
+                                  height: itemHeight,
+                                ),
+                              ),
+                        ),
+                        buildAddButton(spacing),
+                      ],
+                    );
+                  },
+                ),
+              );
+            },
+          )
+        ],
       ),
     );
   }
 
-  Container buildPlayingNow(double w, double itemHeight, double spacing) {
-    return Container(
-      width: w,
-      height: itemHeight,
-      decoration: buildBoxDecoration(Colors.black),
-      child: GestureDetector(
-        child: Stack(
+  Align buildAddButton(double spacing) {
+    double s = spacing / 2;
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: Container(
+        decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        padding: EdgeInsets.all(s),
+        margin: EdgeInsets.only(bottom: s),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Container(
-                width: w * seekTime,
+            Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            SizedBox(width: spacing / 4),
+            Text(
+              "Add",
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(width: spacing / 4),
+          ],
+        ),
+      ),
+    );
+  }
+
+  LayoutBuilder buildHome() {
+    return LayoutBuilder(
+      builder: (BuildContext buildContext, BoxConstraints boxConstraints) {
+        double k = boxConstraints.maxHeight * 0.075;
+        return Container(
+          margin: EdgeInsets.fromLTRB(k, k, k, 0),
+          child: LayoutBuilder(
+            builder: (bc, c) {
+              double itemHeight = c.maxHeight / 6;
+              double spacing = c.maxHeight / 24;
+              double w = c.maxWidth;
+              return Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        buildCurrentRing(w, itemHeight, spacing / 1.5),
+                        SizedBox(height: spacing),
+                        buildNextRing(w, itemHeight, spacing / 1.5),
+                        SizedBox(height: spacing),
+                        buildSelectCycle(w, itemHeight, spacing),
+                        SizedBox(height: spacing),
+                        buildPlayingNow(w, itemHeight, spacing / 1.5),
+                        SizedBox(height: spacing),
+                        buildPlayerControls(w, itemHeight, spacing),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildPlayingNow(double w, double itemHeight, double spacing) {
+    final Shader linearGradient = LinearGradient(
+      colors: <Color>[
+        Colors.black,
+        Colors.black,
+        Colors.white,
+        Colors.white,
+      ],
+      stops: [
+        0,
+        (w + seekTime + spacing * 3.2) / w,
+        (w + seekTime + spacing * 3.2) / w,
+        1.0,
+      ],
+    ).createShader(Rect.fromLTWH(0.0, 0.0, w, itemHeight));
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: GestureDetector(
+        onHorizontalDragUpdate: (DragUpdateDetails dud) {
+          double temp = seekTime + dud.delta.dx;
+          if ((w + temp >= 0) & (w + temp <= w))
+            setState(() {
+              seekTime = temp;
+            });
+        },
+        child: Container(
+          width: w,
+          height: itemHeight,
+          color: Colors.black,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.bottomRight,
+                padding: EdgeInsets.all(spacing / 8),
+                width: w + seekTime,
                 height: itemHeight,
                 color: secondaryColor,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(spacing),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Playing Now",
-                    style: TextStyle(color: Colors.white),
+                child: Text(
+                  ((w + seekTime) / w).toString().substring(0, 3),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
                   ),
-                  SizedBox(height: spacing / 2),
-                  Text(
-                    "Vaaya en veera",
-                    style: TextStyle(
-                        color: Colors.white, fontSize: itemHeight * 0.3),
-                  )
-                ],
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.all(spacing),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Playing Now",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: spacing / 2),
+                    Text(
+                      "Vaaya en veera",
+                      style: TextStyle(
+                          fontSize: itemHeight * 0.3,
+                          foreground: Paint()..shader = linearGradient),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -155,7 +264,8 @@ class _HomeState extends State<Home> {
         }).toList();
         break;
       case Cycle.HOURS:
-        intervalList = List.generate(23, (i) {
+        if (interval > 23) interval = 23;
+        intervalList = List.generate(24, (i) {
           return i + 1;
         }).map((f) {
           return DropdownMenuItem<int>(
@@ -181,7 +291,6 @@ class _HomeState extends State<Home> {
           );
         }).toList();
         break;
-      default:
     }
 
     return Container(
@@ -194,7 +303,7 @@ class _HomeState extends State<Home> {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Text("Interval", style: TextStyle(color: Colors.white)),
+          Text("Interval", style: TextStyle(color: Colors.white54)),
           SizedBox(height: spacing / 2),
           Theme(
             data: Theme.of(context).copyWith(canvasColor: Colors.black),
@@ -272,26 +381,16 @@ class _HomeState extends State<Home> {
       height: itemHeight,
       padding: EdgeInsets.all(spacing),
       decoration: buildBoxDecoration(Colors.black),
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Next Ringtone", style: TextStyle(color: Colors.white70)),
-              SizedBox(height: spacing / 2),
-              Text(
-                "Malargale Malargale",
-                style:
-                    TextStyle(fontSize: itemHeight * 0.3, color: Colors.white),
-              )
-            ],
-          ),
-          Expanded(child: Container()),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.info_outline),
-          ),
+          Text("Next Ringtone", style: TextStyle(color: Colors.white70)),
+          SizedBox(height: spacing / 2),
+          Text(
+            "Malargale Malargale",
+            style: TextStyle(fontSize: itemHeight * 0.3, color: Colors.white),
+          )
         ],
       ),
     );
@@ -303,25 +402,16 @@ class _HomeState extends State<Home> {
       width: w,
       height: itemHeight,
       decoration: buildBoxDecoration(Colors.white),
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Current Ringtone", style: TextStyle(color: Colors.black54)),
-              SizedBox(height: spacing / 2),
-              Text(
-                "Kannathil Muthamittal",
-                style: TextStyle(fontSize: itemHeight * 0.3),
-              )
-            ],
-          ),
-          Expanded(child: Container()),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.info_outline),
-          ),
+          Text("Current Ringtone", style: TextStyle(color: Colors.black54)),
+          SizedBox(height: spacing / 2),
+          Text(
+            "Kannathil Muthamittal",
+            style: TextStyle(fontSize: itemHeight * 0.3),
+          )
         ],
       ),
     );
