fd6db077e2dc5a06d629098766b3473e6ff0a9ee
Seshan <zeshan.nandan@gmail.com>
2019-05-14 16:10:21 +0530

    Still in home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:demo/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,6 +12,9 @@ enum Cycle { MINUTES, HOURS, DAYS }
 class _HomeState extends State<Home> {
   int currTab = 0;
   Cycle cycle = Cycle.MINUTES;
+  int interval = 10;
+
+  double seekTime = 0.3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,6 +85,39 @@ class _HomeState extends State<Home> {
       width: w,
       height: itemHeight,
       decoration: buildBoxDecoration(Colors.black),
+      child: GestureDetector(
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                width: w * seekTime,
+                height: itemHeight,
+                color: secondaryColor,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(spacing),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Playing Now",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: spacing / 2),
+                  Text(
+                    "Vaaya en veera",
+                    style: TextStyle(
+                        color: Colors.white, fontSize: itemHeight * 0.3),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -91,56 +128,138 @@ class _HomeState extends State<Home> {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(spacing / 1.5),
-            height: itemHeight,
-            width: w / 2 - spacing / 2,
-            decoration: buildBoxDecoration(Colors.black),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Cycle", style: TextStyle(color: Colors.white54)),
-                SizedBox(height: spacing / 2),
-                DropdownButton<Cycle>(
-                  isDense: true,
-                  value: cycle,
-                  isExpanded: true,
-                  onChanged: (value) {
-                    setState(() {
-                      cycle = value;
-                    });
-                  },
-                  items: Cycle.values.toList().map((Cycle value) {
-                    String cycleString = "Minutes";
-                    switch (value) {
-                      case Cycle.MINUTES:
-                        cycleString = "Minutes";
-                        break;
-                      case Cycle.HOURS:
-                        cycleString = "Hours";
-                        break;
-                      case Cycle.DAYS:
-                        cycleString = "Days";
-                        break;
-                    }
-                    return DropdownMenuItem<Cycle>(
-                      value: value,
-                      child: Text(cycleString,
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                    );
-                  }).toList(),
-                ),
-              ],
+          buildCycle(spacing, itemHeight, w),
+          SizedBox(width: spacing),
+          buildInterval(spacing, itemHeight, w),
+        ],
+      ),
+    );
+  }
+
+  Container buildInterval(double spacing, double itemHeight, double w) {
+    List<DropdownMenuItem<int>> intervalList;
+
+    switch (cycle) {
+      case Cycle.MINUTES:
+        if (interval % 10 != 0) interval += 10 - interval % 10;
+        intervalList = List.generate(5, (i) {
+          return (i + 1) * 10;
+        }).map((f) {
+          return DropdownMenuItem<int>(
+            value: f,
+            child: Text(
+              f.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        }).toList();
+        break;
+      case Cycle.HOURS:
+        intervalList = List.generate(23, (i) {
+          return i + 1;
+        }).map((f) {
+          return DropdownMenuItem<int>(
+            value: f,
+            child: Text(
+              f.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        }).toList();
+        break;
+      case Cycle.DAYS:
+        if (interval > 15) interval = 15;
+        intervalList = List.generate(15, (i) {
+          return i + 1;
+        }).map((f) {
+          return DropdownMenuItem<int>(
+            value: f,
+            child: Text(
+              f.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        }).toList();
+        break;
+      default:
+    }
+
+    return Container(
+      padding: EdgeInsets.all(spacing / 1.5),
+      height: itemHeight,
+      width: w / 2 - spacing / 2,
+      decoration: buildBoxDecoration(Colors.black),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text("Interval", style: TextStyle(color: Colors.white)),
+          SizedBox(height: spacing / 2),
+          Theme(
+            data: Theme.of(context).copyWith(canvasColor: Colors.black),
+            child: DropdownButton<int>(
+              isDense: true,
+              value: interval,
+              isExpanded: true,
+              onChanged: (value) {
+                setState(() {
+                  interval = value;
+                });
+              },
+              items: intervalList,
             ),
           ),
-          SizedBox(width: spacing),
-          Container(
-            height: itemHeight,
-            width: w / 2 - spacing / 2,
-            decoration: buildBoxDecoration(Colors.black),
+        ],
+      ),
+    );
+  }
+
+  Container buildCycle(double spacing, double itemHeight, double w) {
+    return Container(
+      padding: EdgeInsets.all(spacing / 1.5),
+      height: itemHeight,
+      width: w / 2 - spacing / 2,
+      decoration: buildBoxDecoration(Colors.black),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Cycle", style: TextStyle(color: Colors.white54)),
+          SizedBox(height: spacing / 2),
+          Theme(
+            data: Theme.of(context).copyWith(canvasColor: Colors.black),
+            child: DropdownButton<Cycle>(
+              isDense: true,
+              value: cycle,
+              isExpanded: true,
+              onChanged: (value) {
+                setState(() {
+                  cycle = value;
+                });
+              },
+              items: Cycle.values.toList().map((Cycle value) {
+                String cycleString = "Minutes";
+                switch (value) {
+                  case Cycle.MINUTES:
+                    cycleString = "Minutes";
+                    break;
+                  case Cycle.HOURS:
+                    cycleString = "Hours";
+                    break;
+                  case Cycle.DAYS:
+                    cycleString = "Days";
+                    break;
+                }
+                return DropdownMenuItem<Cycle>(
+                  value: value,
+                  child: Text(cycleString,
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
+                );
+              }).toList(),
+            ),
           ),
         ],
       ),
